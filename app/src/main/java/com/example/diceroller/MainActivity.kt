dd4d3137083cfb3db5d698a05f624dd7e22c7ba4
Aditya <lit2020023@iiitl.ac.in>
2021-02-15 16:16:14 +0530

    Dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,18 +13,18 @@ class MainActivity : AppCompatActivity() {
 
         //start coding here...
         val diceImageView=findViewById<ImageView>(R.id.dice_imageview)
-        diceImageView.setImageResource(R.drawable.dice1)
+        diceImageView.setImageResource(R.drawable.dice_1)
 
         val rollbutton= findViewById<Button>(R.id.roll_button)
         rollbutton.setOnClickListener {
             val randomNumber= Random().nextInt(6)
             when(randomNumber){
-                0 -> diceImageView.setImageResource(R.drawable.dice1)
-                1 -> diceImageView.setImageResource(R.drawable.dice2)
-                2 -> diceImageView.setImageResource(R.drawable.dice3)
-                3 -> diceImageView.setImageResource(R.drawable.dice4)
-                4 -> diceImageView.setImageResource(R.drawable.dice5)
-                5 -> diceImageView.setImageResource(R.drawable.dice6)
+                0 -> diceImageView.setImageResource(R.drawable.dice_1)
+                1 -> diceImageView.setImageResource(R.drawable.dice_2)
+                2 -> diceImageView.setImageResource(R.drawable.dice_3)
+                3 -> diceImageView.setImageResource(R.drawable.dice_4)
+                4 -> diceImageView.setImageResource(R.drawable.dice_5)
+                5 -> diceImageView.setImageResource(R.drawable.dice_6)
             }
         }
     }
